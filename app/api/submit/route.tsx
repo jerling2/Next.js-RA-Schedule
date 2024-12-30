ea17164fd450b1ef7ba54a411efdbd8c7cf80205
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2024-12-30 12:54:24 -0800

    Server writes data files

--- app/api/submit/route.tsx
+++ app/api/submit/route.tsx
@@ -1,17 +1,33 @@
 "use server";
 import { NextRequest } from "next/server";
+import fs from 'fs';
+import path from 'path';
 
 export async function POST(request: NextRequest) {
-    const data = await request.json();
-    console.log(data);
-    return new Response(
-        JSON.stringify(
+    try {
+        const data = await request.json();
+        const now = new Date();
+        const timestamp = now.toISOString().split('.')[0]; // ISO string w.out milliseconds.
+        const filename = data['uoid'] + '-ISO' + timestamp.replace(/:/g, '-') + '.json';
+        const filePath = path.join(process.cwd(), 'data', filename);
+        await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2));
+        return new Response(
+            JSON.stringify({
+                message: 'File written sucessfully',
+            }),
             {
-                message: 'Data received sucessfully!'
+                status: 200,
             }
-        ), 
-        {
-            status: 200, 
-        }
-    );    
+        );
+    } catch (err) {
+        return new Response(
+            JSON.stringify({
+                message: 'Failed to write file to the server.',
+                error: err,
+            }),
+            {
+                status: 500,
+            }
+        )
+    }        
 }   
