0911709f54e1fd6e6669959761a78b18ff8b91f6
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2024-12-30 22:03:01 -0800

    Add redirect button on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 "use client";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div>test</div>
+    <div className="flex h-screen justify-center">
+      <div className="relative w-1/4 flex flex-col place-items-center justify-center p-4 self-stretch">
+        <button 
+          type="button" 
+          className="select-none cursor-pointer mt-1 block w-full px-3 py-2 bg-sky-500 border border-sky-400 rounded-md text-sm shadow-sm font-bold text-white
+          hover:bg-sky-600 hover:border-sky-500 transition-all ease-in-out duration-200"
+          onClick={() => router.push('/form')}>
+          Get Started
+        </button>
+      </div>
+    </div>
   );
 }
