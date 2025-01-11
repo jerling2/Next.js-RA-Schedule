f5e77a8694833140e5821379981f47ee7fe47ceb
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2025-01-10 19:14:42 -0800

    Add 'get started' screen to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,13 +3,17 @@ import DashboardHeader from "@/components/DashboardHeader";
 import PopUpMenu from "@/components/PopUpMenu";
 import ThreeVerticalDots from "@/public/icons/threeVerticalDots.svg"
 import Card from "@/components/Card";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Done from "@/public/icons/done.svg";
 import Pending from "@/public/icons/pending.svg";
+import CreatePage from "@/public/icons/createPage.svg";
 
 export default function Dashboard() {
+    const router = useRouter();
     const [currentTarget, setCurrentTarget] = useState<EventTarget & HTMLElement | undefined>(undefined);
     const [approvalStatus, setApprovalStatus] = useState<string>("approved");
+    const [display, setDisplay] = useState<boolean>(false);
 
     const handlePopUpMenuRender = (render: boolean) => {
         if (!render) {
@@ -20,6 +24,24 @@ export default function Dashboard() {
     return (
         <>
             <DashboardHeader />
+            {!display && <>
+                <div className="relative mt-16 [&>.card]:drop-shadow-lg [&>.card]:bg-white [&>.card]:rounded-md [&>.card>.row>*>*]:py-3">
+                <Card>
+                    <div className="flex flex-row justify-center w-full text-lg font-bold bg-slate-100">
+                        <div>Get Started</div>
+                    </div>
+                    <div className="flex flex-row justify-center items-center w-full font-bold bg-white h-24">
+                        <div className="relative p-2 hover:bg-slate-100 rounded-full aspect-square h-full cursor-pointer active:bg-slate-200"
+                            onClick={()=>router.push("dashboard/document")}>
+                            <CreatePage className="relative text-sky-500 left-2 h-full aspect-square"/>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+            
+            </>}
+            
+            {display && <>
             <div className="relative flex flex-col mt-16">
                 <div className="[&>.card]:drop-shadow-lg [&>.card]:bg-white [&>.card]:rounded-md [&>.card>.row>*>*]:py-3">
                     <Card>
@@ -29,7 +51,7 @@ export default function Dashboard() {
                             <div>Approval Status</div>
                         </div>
                         <div className="grid grid-rows-[3rem] grid-cols-4 items-center w-full font-base bg-white">
-                            <div>Winter Schedule 2024</div>
+                            <div>Winter Term 2025</div>
                             <div>1/6/2</div>
                             <div className="relative h-full justify-self-start flex items-center justify-start aspect-square">
                                 {approvalStatus==="pending" && <>
@@ -60,6 +82,7 @@ export default function Dashboard() {
                     Edit
                 </div>
             </PopUpMenu>
+            </>}
         </>
     );
 }
